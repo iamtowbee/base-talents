24ddda02d18e0d6ad7cedff3fe047bb286273e6f
Oluwatobi Oluyede <oluwatobioluyede4@gmail.com>
2024-10-15 20:18:58 +0100

    Fix Submit button layout

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -14,54 +14,56 @@ import type {
   TransactionResponse,
 } from '@coinbase/onchainkit/transaction';
 import type { Address, ContractFunctionParameters } from 'viem';
+import { toast, useToast } from "@/hooks/use-toast";
 
 interface SubmitButtonProps {
-    token: Address;
-    amount: number;
-    claims: number;
-  }
+  token: Address;
+  amount: number;
+  claims: number;
+}
 
-export default function SubmitButton({ token, amount, claims }: SubmitButtonProps ) {
-    const [isSubmitting, setIsSubmitting] = React.useState(false);
+export default function SubmitButton({
+  token,
+  amount,
+  claims,
+}: SubmitButtonProps) {
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
 
-    const contracts = [
-      {
-        address: PathnerAddress,
-        abi: PathnerABI,
-        functionName: "createBounties",
-        args: [token, amount, claims],
-      },
-    ] as unknown as ContractFunctionParameters[];
-  
-    const handleError = (err: TransactionError) => {
-      console.error('Transaction error:', err);
-    };
-  
-    const handleSuccess = (response: TransactionResponse) => {
-      console.log('Transaction successful', response);
-    };
-  
-    return (
-      <div className="flex w-[450px]">
-        <Transaction
-            contracts={contracts}
-              className="w-[450px]"
-              // capabilities={capabilities}
-              chainId={84532}
-              onError={handleError}
-              onSuccess={handleSuccess} 
+  const contracts = [
+    {
+      address: PathnerAddress,
+      abi: PathnerABI,
+      functionName: "createBounties",
+      args: [token, amount, claims],
+    },
+  ] as unknown as ContractFunctionParameters[];
 
-              >
-              <TransactionButton
-                className="w-full"
-                disabled={isSubmitting}
-                text="Submit"
-              />
-              <TransactionStatus>
-              <TransactionStatusLabel />
-              <TransactionStatusAction />
-              </TransactionStatus>
-            </Transaction> 
-      </div>
-    );
+  const handleError = (err: TransactionError) => {
+    console.error("Transaction error:", err);
+  };
+
+  const handleSuccess = (response: TransactionResponse) => {
+    console.log("Transaction successful", response);
+  };
+
+  return (
+    <Transaction
+      contracts={contracts}
+      className=""
+      // capabilities={capabilities}
+      chainId={84532}
+      onError={handleError}
+      onSuccess={handleSuccess}
+    >
+      <TransactionButton
+        className="h-9 px-4 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90"
+        disabled={isSubmitting}
+        text="Submit Transaction"
+      />
+      <TransactionStatus>
+        <TransactionStatusLabel />
+        <TransactionStatusAction />
+      </TransactionStatus>
+    </Transaction>
+  );
 }
