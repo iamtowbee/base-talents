6b2db5cceb915b7f95f4507884f2a33b5d48fc3d
Sheys <95696361+Sheys11@users.noreply.github.com>
2024-10-20 17:50:50 -0700

    add smart contracts

---
+++ smartcontracts/hardhat.config.ts
@@ -0,0 +1,81 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+import "hardhat-abi-exporter";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+require('dotenv').config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.27",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 888888 
+          },
+        },
+      }
+    ],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    // for mainnet
+    'base-mainnet': {
+      url: `https://base-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    },
+    // for testnet
+    'base-sepolia': {
+      url: `https://base-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    },
+    // for local dev environment
+    'base-local': {
+      url: 'http://localhost:8545',
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    },
+  },
+  etherscan: {
+    //apiKey: process.env.ETHERSCAN_KEY,
+    apiKey: {
+     "base-sepolia": "PLACEHOLDER_STRING"
+    },
+    customChains: [
+      {
+        network: "base-sepolia",
+        chainId: 84532,
+        urls: {
+         apiURL: "https://api-sepolia.basescan.org/api",
+         browserURL: "https://sepolia.basescan.org"
+        }
+      }
+    ]
+  },
+  paths: {
+    sources: "./contracts/",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  abiExporter: {
+    path: "./abis",
+    runOnCompile: true,
+    clear: true,
+    flat: true,
+    pretty: false,
+    except: ["test*", "IERC20.sol"],
+  },
+  gasReporter: {
+    enabled: !!process.env.REPORT_GAS,
+    excludeContracts: ["test*"],
+  },
+};
+
+export default config;
